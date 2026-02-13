747bc43284b84bd467fd50c5d2f34739c79ddf98
Jack Christensen <jack@jackchristensen.com>
2026-02-13 00:27:44 +0000

    vite listens on 0.0.0.0
    So it is accessible from outside the devcontainer.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
+		host: '0.0.0.0',
 		proxy: {
 			'/api': {
 				target: 'http://localhost:4000',

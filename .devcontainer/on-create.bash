139e9792c29b7a23b3028dc7b4b3b485d70afcfe
Jack Christensen <jack@jackchristensen.com>
2026-05-11 15:14:08 +0000

    Use mise to manage more tools

--- .devcontainer/on-create.bash
+++ .devcontainer/on-create.bash
@@ -5,8 +5,5 @@ sudo chown vscode:vscode /persist/local /persist/shared
 mkdir -p /persist/shared/{claude,atuin/{config,data},mise/{data,cache},psql,devcontainer-downloads,playwright}
 
 SCRIPTDIR=$(dirname -- "$(readlink -f -- "$0")")
-"$SCRIPTDIR/fd/install"
-"$SCRIPTDIR/rg/install"
 "$SCRIPTDIR/tern/install"
 "$SCRIPTDIR/verna/install"
-"$SCRIPTDIR/watchexec/install"
